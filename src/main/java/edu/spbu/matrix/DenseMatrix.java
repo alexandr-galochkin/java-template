e2bcbc14c3e0d43573870de9bb9768cf0b53dbe0
alexandr-galochkin <st063746@student.spbu.ru>
2018-09-27 12:34:14 +0300

    wow

--- src/main/java/edu/spbu/matrix/DenseMatrix.java
+++ src/main/java/edu/spbu/matrix/DenseMatrix.java
@@ -1,16 +1,37 @@
 package edu.spbu.matrix;
-
+import java.io.*;
+import java.util.ArrayList;
 /**
  * Плотная матрица
  */
 public class DenseMatrix implements Matrix
 {
+  public int length = 0, hight = 0;
+  public ArrayList<double[]> val = new ArrayList<double[]>();
   /**
    * загружает матрицу из файла
    * @param fileName
    */
   public DenseMatrix(String fileName) {
-
+    try(FileReader file = new FileReader(fileName)){
+      int r = 0;
+      while ((r != -1)&&(r != '\n')){
+        length++;
+      }
+      file.reset();
+      r = 0;
+      while (r != -1){
+        hight++;
+        val.add(new double[length]);
+        int i = 0;
+        while ((r != -1) && (r != '\n')){
+          val.get(hight)[i] = r;
+        }
+      }
+    }
+    catch (IOException ex){
+      System.out.println("Ошибка чтения файла");
+    }
   }
   /**
    * однопоточное умнджение матриц
@@ -42,6 +63,17 @@ public class DenseMatrix implements Matrix
    */
   @Override public boolean equals(Object o) {
     return false;
+  }
+  @Override public String toString() {
+    StringBuilder dat = new StringBuilder();
+    for (int i = 0; i < this.hight; i++){
+      for (int j = 0; j < this.length; j++){
+        dat.append(this.get(i)[j].toString());
+        dat.append(" ");
+      }
+      dat.append("\n");
+    }
+    return (dat.toString());
   }
 
 }
