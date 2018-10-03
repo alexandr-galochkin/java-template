bb35d60883caeb136674d140120098f8a510f079
alexandr-galochkin <43029653+alexandr-galochkin@users.noreply.github.com>
2018-10-03 15:35:53 +0300

    3/8

--- src/test/java/edu/spbu/matrix/MatrixTest.java
+++ src/test/java/edu/spbu/matrix/MatrixTest.java
@@ -10,10 +10,20 @@ public class MatrixTest
    * ожидается 4 таких теста
    */
   @Test
+  public void equal(){
+    Matrix m1 = new DenseMatrix("test.src\\m1.txt");
+    Matrix e = new DenseMatrix("test.src\\e.txt");
+    Matrix m3 = new DenseMatrix("test.src\\m3.txt");
+    assertEquals(false, m1.equals(e));
+    assertEquals(true, m1.equals(m3));
+  }
+  @Test
   public void mulDD() {
-    Matrix m1 = new DenseMatrix("m1.txt");
-    Matrix m2 = new DenseMatrix("m2.txt");
-    Matrix expected = new DenseMatrix("result.txt");
+    Matrix m1 = new DenseMatrix("test.src\\m1.txt");
+    Matrix e = new DenseMatrix("test.src\\e.txt");
+    Matrix m2 = new DenseMatrix("test.src\\m2.txt");
+    Matrix expected = new DenseMatrix("test.src\\rezult.txt");
     assertEquals(expected, m1.mul(m2));
+    assertEquals(null, m2.mul(e));
   }
 }
